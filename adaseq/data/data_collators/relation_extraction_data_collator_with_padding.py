620baf562a629528ae94f4d04c861dd02cd00087
linzhang.zx <linzhang.zx@alibaba-inc.com>
2022-11-18 15:16:57 +0800

    DataCollatorWithPadding.padding return batch

--- adaseq/data/data_collators/relation_extraction_data_collator_with_padding.py
+++ adaseq/data/data_collators/relation_extraction_data_collator_with_padding.py
@@ -1,6 +1,5 @@
 # Copyright (c) Alibaba, Inc. and its affiliates.
 from dataclasses import dataclass
-from typing import Any, Dict, List
 
 from adaseq.data.constant import PAD_LABEL_ID
 from adaseq.metainfo import DataCollators
@@ -14,17 +13,3 @@ class RelationExtractionDataCollatorWithPadding(DataCollatorWithPadding):
     """Relation Extraction collator."""
 
     pad_label_id: int = PAD_LABEL_ID
-
-    def __init__(self, tokenizer, **kwargs):
-        super().__init__(tokenizer)
-
-    def padding(
-        self,
-        batch: Dict[str, Any],
-        fields: List[str],
-        batch_size: int,
-        max_length: int,
-        padding_side: str,
-    ) -> Dict[str, Any]:
-        """do noting"""
-        return batch
